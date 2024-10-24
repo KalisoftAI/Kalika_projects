960d20332271fcd29a70ec5a8cf72b20d1967540
KalikaML <kalisoft308@gmail.com>
2024-10-24 20:02:01 +0530

    Update app.py

--- Kalika_projects-main/Kalika E-commerce/app.py
+++ Kalika_projects-main/Kalika E-commerce/app.py
@@ -101,7 +101,7 @@ def register():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        mobile = request.form['mobile']
+        # mobile = request.form['mobile']
         password = request.form['password']
         confirm_password = request.form['confirm-password']
 
@@ -111,16 +111,21 @@ def register():
 
         connection = get_db_connection()
         cursor = connection.cursor()
-        cursor.execute(("SELECT * FROM users WHERE email = %s OR mobile = %s"), (email, mobile))
+        # cursor.execute("SELECT * FROM users")
+
+        # Check if user already exists
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email, ))
         existing_user = cursor.fetchone()
 
+        # existing_user = cursor.fetchall()
+
         if existing_user:
-            flash('Email or mobile number already exists!')
+            flash('Email already exists!')
             cursor.close()
             connection.close()
-            return redirect(url_for('register'))
+            return redirect(url_for('login'))
         
-        print(f"Inserting user: Name={name}, Email={email}, Mobile={mobile}, Password={password}")
+        print(f"Inserting user: Name={name}, Email={email}, Password={password}")
 
         cursor.execute(("INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)"),
                        (name, email, password))  # Store a hashed password in production!
@@ -129,10 +134,10 @@ def register():
         cursor.close()
         connection.close()
 
-        flash('Registration successful! You can now log in.')
+        # flash('Registration successful! You can now log in.')
         return redirect(url_for('login'))
 
-    return render_template('register.html')
+    return render_template('test_register.html')
 
 # Login route
 # Login route
